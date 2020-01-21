22896cdadaca1f7d1b0b92f22ab142645ab59e4a
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-21 23:31:56 +1100

    refined working cut

--- src/com/codewardev/Persist.java
+++ src/com/codewardev/Persist.java
@@ -7,10 +7,9 @@ import java.util.Arrays;
 public class Persist {
 
 	public static int persistence(long n) {
-		int b = 0;
-		while(String.valueOf(n).length() > 1) {
+		int b;
+		for(b=0; String.valueOf(n).length() > 1; b++) {
 			n= Arrays.stream(String.valueOf(n).split("")).mapToLong(Long::parseLong).reduce(1, (x,y)->x*y);
-			b++;
 		}
 
 		return b;
