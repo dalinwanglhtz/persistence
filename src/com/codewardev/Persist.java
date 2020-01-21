5ab6f00a44bdc2f42128de1c0c9d0c910c6f7b31
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-21 21:35:02 +1100

    first working cut

--- src/com/codewardev/Persist.java
+++ src/com/codewardev/Persist.java
@@ -1,12 +1,19 @@
 package com.codewardev;
 
+import java.util.Arrays;
+
 // https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/java
 
 public class Persist {
 
 	public static int persistence(long n) {
-		
-		return 0;
+		int b = 0;
+		while(String.valueOf(n).length() > 1) {
+			n= Arrays.stream(String.valueOf(n).split("")).mapToLong(Long::parseLong).reduce(1, (x,y)->x*y);
+			b++;
+		}
+
+		return b;
 	}
 
 }
