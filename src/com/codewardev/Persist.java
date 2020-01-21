c3a50d815b905bec814fdbf0f8dc81a69b739dfa
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-21 21:11:13 +1100

    started

---
+++ src/com/codewardev/Persist.java
@@ -0,0 +1,12 @@
+package com.codewardev;
+
+// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/java
+
+public class Persist {
+
+	public static int persistence(long n) {
+		
+		return 0;
+	}
+
+}
